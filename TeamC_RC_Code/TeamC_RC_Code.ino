35564a56ee2e1106e5b0f74bb345936862cad87a
Clint Best <clintguy45@gmail.com>
2015-06-20 14:10:01 -0500

    Changed the pin variables to const in instead of just int Also did some syntax fixes that I've been meaning to clean up Moved short delay into successful loop. I also gave the FSR a const int pin variable Changed the sensor variable name to fsr to be more unique. For the distance sensor we're using a formula that converts the travel time  of the ping to cm. I set the distance to 153cm (roughly 5 ft). The other two variables added were duration and cm to use with the function at the bottom to convert time traveled to distance using the speed of sound.

--- TeamC_RC_Code/TeamC_RC_Code.ino
+++ TeamC_RC_Code/TeamC_RC_Code.ino
@@ -8,110 +8,94 @@
 
   Date: 6-2-2015
 
-  Revised: 6-3-2015 CB
+  Revised: 6-20-2015 CB
 
 
   Remote controlled skateboard:
-
 
   Connected RC controller across Channel 2 on tramsmitter
 
   Then connect to pin 5 in Arduino Uno R3
 
-  Pressure sensor should be connected to pin 0 for input
+  FSR Pressure sensor (fsr) should be connected to pin 0 for input
+
+  Distance sensor (dss) should be connected to pin 7 for input
 
   */
 
- 
+int rfc;      // Channel input from RF receiver
+int throttle; // Variable for throttle measurement
+const int pwm = 9 ;  //Pin for motor outputs
+const int  pss = 0; //Pin for FSR pressure sensor
+float fsr = 0; // The reading from the FSR square
+const int dss = 7 ; //Pin for distance sensor
+long duration, cm; //Variables for distance measurements
 
- float sensor = 0; // The reading from the FSR square
+// Setup for loop, this will run once.
 
- int CH2;      // Channel input from RF receiver
+void setup() {
 
- int throttle; // Drives Motor Speed
+  pinMode(5, INPUT); // Set input pin for throttle
+  pinMode(pwm, OUTPUT);  //Set control pins to be outputs for PWM
+  analogRead(0);
+  analogWrite(pwm, 0);
+  Serial.begin(9600); // initializes serial communication
 
- int pwm = 9 ;  //PWM control for motor outputs is connected to pin 9
 
-  
+}
 
- // Setup for loop, this will run once.
+// Main code runs here, and runs repeatedtly
 
- void setup() {
+void loop() {
 
-   
+  fsr = analogRead(pss); //reads  load fsr input
 
-   pinMode(5, INPUT); // Set input pin for throttle
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  pinMode(dss, OUTPUT);
+  digitalWrite(dss, LOW);
+  delayMicroseconds(2);
+  digitalWrite(dss, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(dss, LOW);
 
-   pinMode(pwm, OUTPUT);  //Set control pins to be outputs for PWM
+  // Using same pin to read returned signal from dss
+  pinMode(dss, INPUT);
+  duration = pulseIn(dss, HIGH);
 
-   analogRead(0);
+  // Convert the time into cm
+  cm = microsecondsToCentimeters(duration);
 
-   analogWrite(pwm, 0);
+  // Check if the weight on the fsr is within the acceptable range
+  // This should cover if there isn't enough pressure (user falls off a small child)
+  // This should also cover if the user is too big (outside what the motor can handle)
+  // Also check if distance fsr sees something within 5 feet.
 
-   // For debugging : will send to Serial monitor
+  if (fsr > 700 && fsr < 900 && cm < 153 ) {
 
-   // uncomment to use
+    rfc = pulseIn(5, HIGH, 25000); // Channel for Throttle Trigger
+    throttle = map(rfc, 1000, 2000, -500, 500); //center over zero
+    throttle = constrain(throttle, -255, 255);
+    throttle = abs(throttle); //Pass Only Absolute PWM Values
+    analogWrite(pwm, throttle);
+    delay(100);
 
-   /*
+  }
 
-   Serial.begin(9600);
+  // Otherwise ignore signals from controller
+  else {
 
-   */
+    analogWrite(pwm, 0);
+    delay(1000); //waits 1 second delay(ms)
 
- }
+  }
 
- // Main code runs here, and runs repeatedtly
+}
 
- void loop() {
-
-   
-
-   sensor = analogRead(0); //reads  load sensor input  
-
-   delay(100);
-
-   // For debugging: uncomment to use
-
-   /*
-
-     Serial.print("Analog reading = ");
-
-     Serial.print(sens); // raw analog reading
-
-   */
-
-   
-
-   // check if the weight on the sensor is within the acceptable range
-
-   // This should cover if there isn't enough pressure (user falls off a small child)
-
-   // This should also cover if the user is too big (outside what the motor can handle)
-
-   if (sensor > 700 || sensor <900){ 
-
-     CH2 = pulseIn(5, HIGH, 25000); // Channel for Throttle Trigger
-
-     
-
-     throttle = map(CH2, 1000,2000, -500, 500); //center over zero
-
-     throttle = constrain(throttle, -255, 255);
-
-     throttle=abs(throttle); //Pass Only Absolute PWM Values
-
-     analogWrite(pwm, throttle);
-
-     
-
-   }
-
-   else {
-
-     analogWrite(pwm,0);
-
-     delay(1000); //waits 1 second delay(ms)
-
-        }
-
- }
+// function that converts time to CM
+long microsecondsToCentimeters(long microseconds)
+{
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
+}
